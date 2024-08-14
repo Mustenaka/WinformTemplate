d1d77c922b3c2d70447cdb897aaa32c6efdb389d
Mustenaka_Win <mumten120@outlook.com>
2024-08-14 21:38:17 +0800

    add ui(temp)

---
+++ WinformTemplate/UI/Verify/UCPage_Verify.Designer.cs
@@ -0,0 +1,44 @@
+﻿namespace WinformTemplate.UI.Verify
+{
+    partial class UCPage_Verify
+    {
+        /// <summary> 
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region 组件设计器生成的代码
+
+        /// <summary> 
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            SuspendLayout();
+            // 
+            // UCPage_Verify
+            // 
+            AutoScaleDimensions = new SizeF(11F, 24F);
+            AutoScaleMode = AutoScaleMode.Font;
+            Name = "UCPage_Verify";
+            Size = new Size(1150, 600);
+            ResumeLayout(false);
+        }
+
+        #endregion
+    }
+}
