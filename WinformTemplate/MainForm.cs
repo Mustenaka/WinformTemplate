86f97b2e5102948b390722a5f43d598b6b35df0d
Mustenaka <mumten120@outlook.com>
2024-11-07 14:09:18 +0800

    update MainForm resize

--- WinformTemplate/MainForm.cs
+++ WinformTemplate/MainForm.cs
@@ -14,8 +14,24 @@ namespace WinformTemplate
         /// <param name="e"></param>
         private void MainForm_SizeChanged(object sender, EventArgs e)
         {
+            //var dis = Math.Abs(this.Size.Height - Lab_Console.Size.Height);
+            //SContainer_Main.SplitterDistance = dis;
+
+            // 检查窗体是否最小化
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                // 如果最小化，不执行 SplitterDistance 的设置
+                return;
+            }
+
             var dis = Math.Abs(this.Size.Height - Lab_Console.Size.Height);
-            SContainer_Main.SplitterDistance = dis;
+
+            // 在窗体正常或最大化时设置 SplitterDistance
+            var newSplitterDistance = Math.Max(
+                SContainer_Main.Panel1MinSize, Math.Min(SContainer_Main.Width - SContainer_Main.Panel2MinSize, dis)
+            );
+
+            SContainer_Main.SplitterDistance = newSplitterDistance;
         }
     }
 }
